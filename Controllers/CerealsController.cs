2617d4558e1ad33f6dd9d15554b2bb3dc6341b61
Rockandrolldream <sa.jba@specialisterne.com>
2022-08-11 13:16:18 +0200

    test

---
+++ Controllers/CerealsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Databasetest.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Databasetest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CerealsController : Controller
+    {
+        private readonly BallingdatabaseContext _context;
+
+        public CerealsController(BallingdatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetCereals")]
+        public IQueryable<Cereal> AllCereals()
+        {  
+           return _context.Cereals;
+        }
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "Cannot Find ID")]
+        [HttpGet("GetCereal")]
+        public ActionResult<Cereal> CerealsDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound("please give an Id " + id);
+            }
+
+            IQueryable<Cereal> output; 
+            List<Cereal> cereals = new List<Cereal>(); 
+
+            output = _context.Cereals.Where(m => m.Id == id);
+            if (output == null)
+            {
+                return NotFound("ID was not Found" + id);
+            }
+
+            return Ok(output);
+        }
+
+        [HttpGet("GetCerealtypeByparameter")]
+        public IQueryable<Cereal>? GetCerealByparameter(string? name, String? Mfr, String? type, String? calories, String? protein, string? fat, string? sodium, string? fiber, string? carbo, string? sugar, string? potass, string? vitamin, string? shelf, string? weight, string? cups, string? rating)
+        {   
+            List<Cereal> cereals = new List<Cereal>();
+
+            var cereal = _context.Cereals.Where(c => (c.Name == name || name == null)
+                                                    && (c.Mfr == Mfr || Mfr == null) 
+                                                    &&  (c.Type == type || type == null)
+                                                    && (c.Calories == calories || calories == null) 
+                                                    && (c.Protein == protein || protein == null)
+                                                    && (c.Fat == fat || fat == null)
+                                                    && (c.Sodium == sodium || sodium == null)
+                                                    && (c.Fiber == fiber || fiber == null)
+                                                    && (c.Carbo == carbo || carbo == null)
+                                                    && (c.Sugars == sugar || sugar == null)
+                                                    && (c.Potass == potass || potass == null)
+                                                    && (c.Vitamins == vitamin || vitamin == null)
+                                                    && (c.Shelf == shelf || shelf == null)
+                                                    && (c.Weight == weight || weight == null)
+                                                    && (c.Cups == cups|| cups == null)
+                                                    && (c.Rating == rating || rating == null)
+
+            );                                      
+
+            return cereal;
+        }
+
+        [HttpPost("PostRequestByparameter")]
+        public Cereal? CreateCereal([Bind("Name,Mfr,Type,Calories,Protein,Fat,Sodium,Fiber,Carbo,Sugars,Potass,Vitamins,Shelf,Weight,Cups,Rating")] Cereal cereal)
+        {
+            if (cereal != null)
+            {
+                _context.Add(cereal);
+                _context.SaveChanges(); 
+                return cereal;
+            }
+
+            return null;
+        } 
+
+        [HttpPost("UpdateRequestByparameter")] 
+         public Cereal? UpdateCereal([Bind("Id,Name,Mfr,Type,Calories,Protein,Fat,Sodium,Fiber,Carbo,Sugars,Potass,Vitamins,Shelf,Weight,Cups,Rating")] Cereal cereal)
+        {  
+           var output = (_context.Cereals?.Where(e => e.Id == cereal.Id));
+
+            if ( output != null)
+            {
+                _context.Update(cereal);
+                _context.SaveChanges();
+                return cereal;
+            }
+
+            return null;
+        }
+
+
+        [HttpDelete("Delete")]
+        public Cereal? DeleteCereal(int id)
+        {
+              var cereals = _context.Cereals.Find(id);
+
+            if (cereals == null)
+            {
+                return null;
+            }
+
+            _context.Cereals.Remove(cereals);
+            _context.SaveChanges();
+
+            return cereals;
+        }
+    }
+}
---
+++ Models/BallingdatabaseContext.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Databasetest.Models
+{
+    public partial class BallingdatabaseContext : DbContext
+    {
+        public BallingdatabaseContext()
+        {
+        }
+
+        public BallingdatabaseContext(DbContextOptions<BallingdatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cereal> Cereals { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Ballingdatabase;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cereal>(entity =>
+            {
+                entity.ToTable("cereal");
+
+                entity.Property(e => e.Calories)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("calories");
+
+                entity.Property(e => e.Carbo)
+                    .HasMaxLength(5)
+                    .IsUnicode(false)
+                    .HasColumnName("carbo");
+
+                entity.Property(e => e.Cups)
+                    .HasMaxLength(5)
+                    .IsUnicode(false)
+                    .HasColumnName("cups");
+
+                entity.Property(e => e.Fat)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("fat");
+
+                entity.Property(e => e.Fiber)
+                    .HasMaxLength(5)
+                    .IsUnicode(false)
+                    .HasColumnName("fiber");
+
+                entity.Property(e => e.Mfr)
+                    .HasMaxLength(11)
+                    .IsUnicode(false)
+                    .HasColumnName("mfr");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(38)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Potass)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("potass");
+
+                entity.Property(e => e.Protein)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("protein");
+
+                entity.Property(e => e.Rating)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("rating");
+
+                entity.Property(e => e.Shelf)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("shelf");
+
+                entity.Property(e => e.Sodium)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("sodium");
+
+                entity.Property(e => e.Sugars)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("sugars");
+
+                entity.Property(e => e.Type)
+                    .HasMaxLength(11)
+                    .IsUnicode(false)
+                    .HasColumnName("type");
+
+                entity.Property(e => e.Vitamins)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("vitamins");
+
+                entity.Property(e => e.Weight)
+                    .HasMaxLength(5)
+                    .IsUnicode(false)
+                    .HasColumnName("weight");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
