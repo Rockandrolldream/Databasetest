86dc35dc4aeca698a798449e3d9d1da5c169e075
Rockandrolldream <sa.jba@specialisterne.com>
2022-08-16 15:25:16 +0200

    update

--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 namespace Databasetest.Models
 {
     public  class User
-    {
+    { 
+        // test
         public string Username { get; set; }
         public string Password { get; set; }
     }
